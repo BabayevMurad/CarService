6d832edd8b657d095fa2fd4ec4b2afde62372054
Babay_aq38@itstep.edu.az <Babay_aq38@itstep.edu.az>
2025-05-19 21:50:33 +0400

    Removed My Mistake

--- CarService.WebApi/Program.cs
+++ CarService.WebApi/Program.cs
@@ -51,8 +51,7 @@ builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
     builder.WithOrigins("http://localhost:5173", "http://localhost:5174", "http://localhost:5175")
            .AllowAnyHeader()
            .AllowAnyMethod()
-           .AllowCredentials()
-           .AllowAnyHeader();
+           .AllowCredentials();
 }));
 
 
