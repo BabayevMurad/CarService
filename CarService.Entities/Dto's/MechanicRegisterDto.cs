9ee2edc143c5407d31eaae140b719c913b9d0658
Babay_aq38@itstep.edu.az <Babay_aq38@itstep.edu.az>
2025-05-24 02:27:02 +0400

    Requeried Delete

--- CarService.Entities/Dto's/MechanicRegisterDto.cs
+++ CarService.Entities/Dto's/MechanicRegisterDto.cs
@@ -4,15 +4,10 @@ namespace CarService.Entities.Dto_s
 {
     public class MechanicRegisterDto
     {
-        [Required]
         public string Name { get; set; }
-        [Required]
         public string Surname { get; set; }
-        [Required]
         public string WorkType { get; set; }
-        [Required]
         public string Username { get; set; }
-        [Required]
         public string Password { get; set; }
 
     }
